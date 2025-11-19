0e527d83ee4dca0c1e6aaf861b424020a9e30d96
Nivekdex1 <nivekpal@gmail.com>
2025-11-19 17:25:55 +0100

    Update video sources in ImpactStoriesPage and HomepageGlowfestVideo components

--- src/imports/HomepageGlowfestVideo.tsx
+++ src/imports/HomepageGlowfestVideo.tsx
@@ -3,7 +3,7 @@ export default function HomepageGlowfestVideo() {
     <div className="relative w-full h-full" data-name="Homepage Glowfest Video">
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="https://cdn1.kingscloud.co/kingscloud/files/67a726c504cf2417588bfd88/cbedb32c-4864-450c-9a26-52c48b06a1ca?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJZ7R2RYYHYMD7OLA%2F20251114%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251114T111358Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=53467b80b4b9bb7e16e83715360a3341d1612ae9be53c42159163c0ea448cef8"
+        src="https://kidspiration.org/videos/KIDSPIRATION_GLOWFEST_HD.mp4"
         controls
         controlsList="nodownload"
         playsInline
