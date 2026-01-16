01b8704444f6ceda49dd59bcb94f9c559f9a499c
Nivekdex1 <nivekpal@gmail.com>
2026-01-16 10:37:45 +0100

    Update HomepageGlowfestVideo.tsx

--- src/imports/HomepageGlowfestVideo.tsx
+++ src/imports/HomepageGlowfestVideo.tsx
@@ -14,3 +14,21 @@ export default function HomepageGlowfestVideo() {
     </div>
   );
 }
+
+
+export default function HomepageImpactVideo() {
+  return (
+    <div className="relative w-full h-full" data-name="Homepage Glowfest Video">
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src="/videos/KIDSPIRATION_IMPACT.mp4"
+        controls
+        controlsList="nodownload"
+        playsInline
+        preload="metadata"
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
